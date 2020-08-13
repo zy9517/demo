cfae0f22d0cca5e3129eaa5e955be6604a19a711
zy9517 <1521322016@qq.com>
2020-08-13 23:30:12 +0800

    Revert "1"
    This reverts commit 070bd88b2aced45e2de42adb5931bfbe174cb0bf.
    
    # Conflicts:
    #	src/test/java/com/itcast/mapper/GitTest.java
    #	src/test/java/com/itcast/mapper/UserMapperTest.java

--- src/test/java/com/itcast/mapper/GitTest.java
+++ src/test/java/com/itcast/mapper/GitTest.java
@@ -1,9 +1,4 @@
 package com.itcast.mapper;
 
 public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("222");
-        System.out.println("333");
-    }
 }
